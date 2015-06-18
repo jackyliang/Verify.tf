578b46e14ccf9d3eca5a735286219ad38c2676c4
Jacky Liang <bluelegopiece@hotmail.com>
2015-06-18 14:53:18 -0400

    updated comments in the test suite

--- ScrapeUGCTest.php
+++ ScrapeUGCTest.php
@@ -2,15 +2,18 @@
 
 require_once("ScrapeUGC.php");
 
+// URLs to scrape
 $ourURL = '/Users/loop/Desktop/BBros.html';
 $theirURL = '/Users/loop/Desktop/enemyTeam.html';
 
 $scrape = new ScrapeUGC($ourURL, $theirURL);
 
+// Print info about our team
 echo "Our team name: " . $scrape->getOurTeamName();
 echo "Our team URL: " . $scrape->getOurTeamURL();
 var_dump($scrape->getOurTeam());
 
+// Print info about their team
 echo "Their team name: " . $scrape->getTheirTeamName();
 echo "Their team URL: " . $scrape->getTheirTeamURL();
 var_dump($scrape->getTheirTeam());
